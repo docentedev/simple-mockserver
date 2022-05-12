b703047f5005f0efa21c8977f80f6570b8374893
Claudio <claudio@MacBook-Pro-de-Claudio-2.local>
2022-05-12 07:34:01 -0400

    feat: update features

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,25 @@ import (
 	"io/fs"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
+type APIHeader struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
 type APIDefinition struct {
-	Url      string `json:"url"`
-	Response string `json:"response"`
+	Url      string      `json:"url"`
+	Response string      `json:"response"`
+	Status   int         `json:"status"`
+	Method   string      `json:"method"`
+	Headers  []APIHeader `json:"headers"`
 }
 
 func readFilesIntoDirectory(dir string) ([]fs.FileInfo, error) {
@@ -53,14 +63,58 @@ func readFile(dir string, name string) (string, error) {
 	return string(content), err
 }
 
-func main() {
-	path := "./services"
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		err := os.Mkdir(path, 0777)
+func createFolderIfNotExists(dir string) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.Mkdir(dir, 0777)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 	}
+	return nil
+}
+
+func raw_connect(host string, ports []string) bool {
+	for _, port := range ports {
+		timeout := time.Second
+		conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
+		if err != nil {
+			//fmt.Println("Connecting error:", err)
+			return false
+		}
+		if conn != nil {
+			defer conn.Close()
+			//fmt.Println("Opened", net.JoinHostPort(host, port))
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	// get port from os.Args --port=<port> or use default
+	port := "8080"
+	// Check args 1
+	if len(os.Args) < 2 {
+		// os.Exit(1)
+	} else {
+		port = os.Args[1]
+	}
+	// is port in use?
+	// if yes, exit
+	// if no, start server
+
+	if port == "" {
+		port = "8080"
+	}
+
+	ifPortInUse := raw_connect("localhost", []string{port})
+	if ifPortInUse {
+		fmt.Println("Port", port, "is in use")
+		os.Exit(1)
+	}
+
+	path := "./services"
+	createFolderIfNotExists(path)
 
 	files, err := readFilesIntoDirectory(path)
 	if err != nil {
@@ -70,7 +124,7 @@ func main() {
 	r := mux.NewRouter()
 
 	// print text with color
-	fmt.Println("\033[1;32m[+]\033[0m", "Starting server on port 8080")
+	fmt.Println("\033[1;32m[+]\033[0m", "Starting server on port "+port)
 	fmt.Println("📂 Serving files from: " + path)
 	fmt.Println("-----------------------------------------------------")
 	fmt.Println("Allows for the following endpoints:")
@@ -83,14 +137,17 @@ func main() {
 		// fmt.Println(fileContent)
 		data := APIDefinition{}
 		_ = json.Unmarshal([]byte(fileContent), &data)
-		fmt.Println("\033[1;32m>\033[0m", data.Url)
+		fmt.Println("\033[1;32m>\033[0m", data.Url, " - ", data.Method, " - ", data.Status)
 		//fmt.Println(data.Response)
 
 		r.HandleFunc(data.Url, func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
+			// assign header from data.headers
+			for _, header := range data.Headers {
+				w.Header().Set(header.Name, header.Value)
+			}
+			w.WriteHeader(data.Status)
 			w.Write([]byte(data.Response))
-		})
+		}).Methods(data.Method)
 	}
 
 	// create static routes
@@ -100,6 +157,7 @@ func main() {
 		w.Write([]byte("{\"message\": \"Hello World\"}"))
 	})
 
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":"+port, r)
+
 	// createFileIfNotExists(path, "service.go", `{}`)
 }
